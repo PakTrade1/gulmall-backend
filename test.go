0cb0d34624ff105bb229e59dc440fea8c22dfaca
Hamayuen <hamayuenabbasi@gmail.com>
2022-10-21 19:28:07 +0500

    hamayuen

---
+++ test.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://hamayuen:abbasi786@appdb.yzs6ynw.mongodb.net/?retryWrites=true&w=majority"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	quickstartDatabase := client.Database("quickstart")
+	podcastsCollection := quickstartDatabase.Collection("podcasts")
+	episodesCollection := quickstartDatabase.Collection("episodes")
+	podcastResult, err := podcastsCollection.InsertOne(ctx, bson.D{
+		{Key: "title", Value: "The Polyglot Developer Podcast"},
+		{Key: "author", Value: "Nic Raboy"},
+	})
+	episodeResult, err := episodesCollection.InsertMany(ctx, []interface{}{
+		bson.D{
+			{"podcast", podcastResult.InsertedID},
+			{"title", "GraphQL for API Development"},
+			{"description", "Learn about GraphQL from the co-creator of GraphQL, Lee Byron."},
+			{"duration", 25},
+		},
+		bson.D{
+			{"podcast", podcastResult.InsertedID},
+			{"title", "Progressive Web Application Development"},
+			{"description", "Learn about PWA development with Tara Manicsic."},
+			{"duration", 32},
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Inserted %v documents into episode collection!\n", len(episodeResult.InsertedIDs))
+
+	coll := client.Database("test").Collection("user")
+	doc := bson.D{{"title", "Invisible Cities"}, {"author", "Italo Calvino"}, {"year_published", 1974}}
+
+	result, err := coll.InsertOne(context.TODO(), doc)
+
+	fmt.Printf("Inserted document with _id: %v\n", result.InsertedID)
+
+}
