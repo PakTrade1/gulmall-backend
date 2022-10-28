e431521399d55637cef0c721d8ff5ed996b29da7
junaid5546 <lafmk22@gmail.com>
2022-10-28 19:42:20 +0400

    new changes

--- services/color.service.go
+++
@@ -1,9 +0,0 @@
-package services
-
-import "example/apies/models"
-
-type ColorService interface {
-	CreateColor(*models.Color) error
-	// GetColor(*string) (*models.Color, error)
-	GetAll() ([]*models.Color, error)
-}
