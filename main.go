873526ea0f9769d9538366ec7a61240fad91b328
Hamayuen <hamayuenabbasi@gmail.com>
2022-11-27 08:44:01 +0500

    v.3

--- main.go
+++ main.go
@@ -24,11 +24,15 @@ func main() {
 	r.HandleFunc("/get-color", color.Color)
 	// r.HandleFunc("/ItemAdd", item.ItemInsertone)
 	r.HandleFunc("/update-item", item.Item_update_one)
-	r.HandleFunc("/get-item", item.Items)
+	r.HandleFunc("/get-item-with-status", item.Items)
 	r.HandleFunc("/get-user", User.Mammals_getall)
 	r.HandleFunc("/add-user", User.Mammals_insertone)
 	r.HandleFunc("/get-user-by-id", User.Mammals_select_one)
 	r.HandleFunc("/update-user", User.Mammals_update_one)
+	r.HandleFunc("/serch-item-by-id", item.Serch_item_by_id)   //item_id
+	r.HandleFunc("/delete-item-by-id", item.Item_delete_by_id) // item_id and status
+	r.HandleFunc("/get-all-item", item.Get_all_items)          // get all items
+
 	r.HandleFunc("/upload-file", storage.UploadFile).Methods("POST")
 	//r.HandleFunc("/delete-file", item.Delte_item).Methods("POST")
 	//r.HandleFunc("/delete-image", storage.Deltefile).Methods("POST")
