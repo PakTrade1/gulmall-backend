ff55fc2bb7cdb1e2977cc7230560b7c5448f284d
junaid5546 <lafmk22@gmail.com>
2023-01-06 20:59:12 +0400

    deleted comments

--- main.go
+++ main.go
@@ -31,17 +31,13 @@ func main() {
 	r.HandleFunc("/get-color", color.Color)
 	r.HandleFunc("/get-size", size.Size)
 	r.HandleFunc("/get-size-chart", size.Size_select_by_child_id)
-
 	r.HandleFunc("/get-payment-service", payment_service.Get_payment_method)
-
-	// r.HandleFunc("/ItemAdd", item.ItemInsertone)
 	r.HandleFunc("/update-item", item.Item_update_one)
 	r.HandleFunc("/get-item-with-status", item.Items)
 	r.HandleFunc("/get-categories", categories.Get_all_categories)
 	r.HandleFunc("/get-sub-categories", categories.Sub_Categories_select_by_Cat_id)
 	r.HandleFunc("/get-child-categories", categories.Child_Categories_select_by__sub_Cat_id)
 	r.HandleFunc("/get-gender", gender.Gender)
-
 	r.HandleFunc("/get-user", User.Mammals_getall)
 	r.HandleFunc("/add-user", User.Mammals_insertone)
 	r.HandleFunc("/get-user-by-id", User.Mammals_select_one)
@@ -50,12 +46,8 @@ func main() {
 	r.HandleFunc("/delete-item-by-id", item.Item_delete_by_id) // item_id and status
 	r.HandleFunc("/get-all-item", item.Get_all_items)          // POST         // get all items
 	r.HandleFunc("/upload-file", storage.UploadFile).Methods("POST")
-	//r.HandleFunc("/delete-file", item.Delte_item).Methods("POST")
-	//r.HandleFunc("/delete-image", storage.Deltefile).Methods("POST")
-
 	r.HandleFunc("/get-all-cart", Allcart.Get_cart_all_with_id_data)
 	r.HandleFunc("/get-cart-with-id", Allcart.Get_cart_with_id)
-	//r.HandleFunc("/update-cart", Allcart.Update_cart_all_with_id_data)
 	r.HandleFunc("/delete-cart", Allcart.Cart_delete)
 	r.HandleFunc("/update-cart-in", Allcart.Update_cart)
 	r.HandleFunc("/upload-filen", blobstorage.UploadFile).Methods("POST")
