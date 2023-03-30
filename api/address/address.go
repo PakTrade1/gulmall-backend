0616f13d5ee725f1a96625e58ad9990dd630ea52
Hamayuen <hamayuenabbasi@gmail.com>
2023-03-30 20:51:41 +0400

    incomplete

--- api/address/address.go
+++ api/address/address.go
@@ -16,7 +16,7 @@ type shipping_mammal_id struct {
 }
 
 type shipping_table struct {
-	Address struct {
+	Address []struct {
 		Name        string `json:"name"`
 		Line_1      string `json:"address_line_1"`
 		Line_2      string `json:"address_line_2"`
@@ -96,46 +96,95 @@ func Add_shipping_address(w http.ResponseWriter, req *http.Request) {
 	}
 
 	coll := docking.PakTradeDb.Collection("Mammals_address")
-
+	var result shipping_table
+	filter := bson.M{"mammal_id": shipping_req.Mammal_id}
+	err1 := coll.FindOne(context.TODO(), filter).Decode(&result)
+	if err1 != nil {
+		fmt.Println("errror retrieving user userid : " + result.Mammal_id)
+	}
+	fmt.Print(shipping_req.Mammal_id)
 	// Requires the MongoDB Go Driver
 	// https://go.mongodb.org/mongo-driver
-	ctx := context.TODO()
-
-	mongo_query := bson.M{
-		"Address": bson.A{
-			bson.M{
-				"name":        shipping_req.Address.Name,
-				"line_1":      shipping_req.Address.Line_1,
-				"line_2":      shipping_req.Address.Line_2,
-				"subrub":      shipping_req.Address.Subrub,
-				"city":        shipping_req.Address.City,
-				"postal_code": shipping_req.Address.Postal_code,
+	if result.Mammal_id != "" {
+		ctx := context.TODO()
+		// Open an aggregation cursor
+		cursor, err := coll.UpdateMany(ctx,
+			bson.M{"mammal_id": shipping_req.Mammal_id},
+			bson.D{
+				{Key: "$push", Value: bson.M{
+					"Address": bson.A{
+						bson.M{
+							"name":        shipping_req.Address[0].Name,
+							"line_1":      shipping_req.Address[0].Line_1,
+							"line_2":      shipping_req.Address[0].Line_2,
+							"subrub":      shipping_req.Address[0].Subrub,
+							"city":        shipping_req.Address[0].City,
+							"postal_code": shipping_req.Address[0].Postal_code,
+						},
+					},
+				}},
 			},
-		},
-		"mammal_id": shipping_req.Mammal_id,
-	}
-	// Open an aggregation cursor
-	cursor, err := coll.InsertOne(ctx, mongo_query)
-	if err != nil {
-		fmt.Fprintf(w, "%s\n", err)
-	}
-	var results respone_struct
-	var resp1 []shipping_table
-	if cursor != nil {
-		results.Status = http.StatusOK
-		results.Message = "success"
+		)
+		if err != nil {
+			fmt.Fprintf(w, "%s\n", err)
+		}
+		var results respone_struct
+		var resp1 []shipping_table
+		if cursor != nil {
+			results.Status = http.StatusOK
+			results.Message = "success"
 
+		} else {
+			results.Message = "decline"
+
+		}
+		results.Data = resp1
+		output, err := json.MarshalIndent(results, "", "    ")
+		if err != nil {
+			panic(err)
+
+		}
+
+		fmt.Fprintf(w, "%s\n", output)
 	} else {
-		results.Message = "decline"
+		ctx := context.TODO()
 
+		mongo_query := bson.M{
+			"Address": bson.A{
+				bson.M{
+					"name":        shipping_req.Address[0].Name,
+					"line_1":      shipping_req.Address[0].Line_1,
+					"line_2":      shipping_req.Address[0].Line_2,
+					"subrub":      shipping_req.Address[0].Subrub,
+					"city":        shipping_req.Address[0].City,
+					"postal_code": shipping_req.Address[0].Postal_code,
+				},
+			},
+			"mammal_id": shipping_req.Mammal_id,
+		}
+		// Open an aggregation cursor
+		cursor, err := coll.InsertOne(ctx, mongo_query)
+		if err != nil {
+			fmt.Fprintf(w, "%s\n", err)
+		}
+		var results respone_struct
+		var resp1 []shipping_table
+		if cursor != nil {
+			results.Status = http.StatusOK
+			results.Message = "success"
+
+		} else {
+			results.Message = "decline"
+
+		}
+		results.Data = resp1
+		output, err := json.MarshalIndent(results, "", "    ")
+		if err != nil {
+			panic(err)
+
+		}
+
+		fmt.Fprintf(w, "%s\n", output)
 	}
-	results.Data = resp1
-	output, err := json.MarshalIndent(results, "", "    ")
-	if err != nil {
-		panic(err)
-
-	}
-
-	fmt.Fprintf(w, "%s\n", output)
 
 }
